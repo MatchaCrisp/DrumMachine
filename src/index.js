d860d9915dfe5e32e18b8114792e144311a13259
MatchaCrisp <junyanbill@hotmail.com>
2021-04-16 21:06:24 -0400

    skeletal

--- src/index.js
+++ src/index.js
@@ -1,17 +1,66 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './index.scss';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const App=()=>{
+  return (
+    <div id="app">
+      <DrumMachine />
+    </div>
+  )
+}
+const DrumMachine=()=>{
+  const [data, setData]=useState({});
+  const [curr, setCurr]=useState('');
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+  return (
+    <div id="drum-machine">
+      <DrumPads />
+      <CtrlPad />
+    </div>
+  )
+}
+
+const CtrlPad=props=>{
+  return (
+    <div id="ctrl-pad">
+      <Power />
+      <Display />
+      <Swap />
+    </div>
+    
+
+  )
+}
+const Power=props=>{
+  return (
+    <button id="power"></button>
+  )
+}
+const Display=props=>{
+  return (
+    <p id="display">
+
+    </p>
+  )
+}
+const Swap=props=>{
+  return (
+    <button id="swap"></button>
+  )
+}
+const DrumPads=props=>{
+  //const renderPads=(id,value)=><DrumPad id={id} key={id} value={value} handleClick={()=>padClick(id)}/>
+  //const jsx=ids.map(id=>renderPads(id[0],id[1]));
+  return (
+    <div id="drum-pads">
+    </div>
+  )
+}
+//const DrumPad=props=>{
+//    return (
+//      <button className="drum-pad" id={props.id} onClick={props.handleClick}>{props.id}</button>
+//    )
+//}
+ReactDOM.render(<App/>, document.getElementById('root'));
