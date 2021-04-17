0d66fbca49bdceb77bd3965b5c30bafbe7c42c6b
MatchaCrisp <junyanbill@hotmail.com>
2021-04-17 19:37:35 -0400

    prelim structure

--- src/index.js
+++ src/index.js
@@ -10,9 +10,65 @@ const App=()=>{
   )
 }
 const DrumMachine=()=>{
-  const [data, setData]=useState({});
+  
   const [curr, setCurr]=useState('');
-  const [url]=useState('');
+  const [power, setPower]=useState(true);
+  //true = 1, false = 2
+  const [bank,setBank]=useState(true);
+
+
+  const powerSwitch=()=>{
+    setPower(!power);
+  }
+
+  const bankSwitch=()=>{
+    setBank(!bank);
+  }
+
+  const handleDisplay=desc=>{
+    setCurr(desc);
+  }
+
+  return (
+    <div id="drum-machine">
+      <DrumPads handleDisplay={handleDisplay} power={power} bank={bank}/>
+      <CtrlPad handlePower={powerSwitch} handleBank={bankSwitch} curr={curr}/>
+    </div>
+  )
+}
+
+const CtrlPad=props=>{
+  return (
+    <div id="ctrl-pad">
+      <Power handlePower={props.powerSwitch}/>
+      <Display curr={props.curr}/>
+      <Swap handleBank={props.bankSwitch}/>
+    </div>
+    
+
+  )
+}
+const Power=props=>{
+  return (
+    <button id="power" onClick={props.handlePower}>Power</button>
+  )
+}
+const Display=props=>{
+  return (
+    <p id="display">
+      {props.curr}
+    </p>
+  )
+}
+const Swap=props=>{
+  return (
+    <button id="swap" onClick={props.handleBank}>Swap</button>
+  )
+}
+const DrumPads=props=>{
+  const [data, setData]=useState({});
+  const [url]=useState('https://raw.githubusercontent.com/MatchaCrisp/DrumMachine/main/src/data/soundBite.json');
+
   useEffect=(()=>{
     if (!url) return;
 
@@ -26,53 +82,29 @@ const DrumMachine=()=>{
 
   },[url]);
 
-  return (
-    <div id="drum-machine">
-      <DrumPads />
-      <CtrlPad />
-    </div>
-  )
-}
 
-const CtrlPad=props=>{
-  return (
-    <div id="ctrl-pad">
-      <Power />
-      <Display />
-      <Swap />
-    </div>
-    
+  const renderPad=items=><DrumPad key ={items.idKey} items={items} power={props.power} bank={props.bank} handleDisplay={props.handleDisplay}/>;
 
-  )
-}
-const Power=props=>{
-  return (
-    <button id="power"></button>
-  )
-}
-const Display=props=>{
-  return (
-    <p id="display">
+  const jsx=data.soundBite.map(items=>renderPad(items));
 
-    </p>
-  )
-}
-const Swap=props=>{
-  return (
-    <button id="swap"></button>
-  )
-}
-const DrumPads=props=>{
-  //const renderPads=(id,value)=><DrumPad id={id} key={id} value={value} handleClick={()=>padClick(id)}/>
-  //const jsx=ids.map(id=>renderPads(id[0],id[1]));
+
   return (
     <div id="drum-pads">
+      {jsx}
     </div>
   )
 }
-//const DrumPad=props=>{
-//    return (
-//      <button className="drum-pad" id={props.id} onClick={props.handleClick}>{props.id}</button>
-//    )
-//}
+const DrumPad=props=>{
+  const sound=props.items;
+  const handleClick=()=>{
+    if (!props.power) return;
+    //play sound and change display
+    props.handleDisplay(props.bank?sound.desc1:sound.desc2);
+    console.log(bank?sound.url1:sound.url2);
+  }
+  //handle keydown event
+    return (
+      <button className="drum-pad" id={sound.id} onClick={handleClick}>{props.id}</button>
+    )
+}
 ReactDOM.render(<App/>, document.getElementById('root'));
